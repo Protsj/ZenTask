fb888f8fbc0b10bca454f229ed5e2e63f66ba969
qrtm <artemprotsenkoyt@gmail.com>
2026-04-14 21:08:26 +0300

    feat: introduce BaseTask abstract class

---
+++ ZenTask.Core/Models/BaseTask.cs
@@ -0,0 +1,20 @@
+﻿namespace ZenTask.Core.Models
+{
+    public abstract class BaseTask //Abstract base class for all tasks, containing common properties and logic
+    {
+        public Guid Id { get; protected set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; protected set; }
+        protected BaseTask(string title, string description = "")
+        {
+            if(string.IsNullOrWhiteSpace(title))
+                throw new ArgumentException($"Title cannot be empty. {nameof(title)}");
+            Id = Guid.NewGuid();
+            Title = title;
+            Description = description;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+    }
+}
