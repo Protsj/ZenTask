1451e9089a923ca3f311b76b0e04c55361beb354
qrtm <artemprotsenkoyt@gmail.com>
2026-04-14 21:06:10 +0300

    feat: add interfaces for classes

---
+++ ZenTask.Core/Interfaces/ICompletable.cs
@@ -0,0 +1,8 @@
+﻿namespace ZenTask.Core.Interfaces
+{
+    public interface ICompletable //Interface for tasks that can be marked as completeds
+    {
+        bool IsCompleted { get; }
+        void Complete();
+    }
+}
---
+++ ZenTask.Core/Interfaces/IRemindable.cs
@@ -0,0 +1,8 @@
+﻿namespace ZenTask.Core.Interfaces
+{
+    public interface IRemindable //Interface for tasks that can have reminders
+    {
+        DateTime ReminderTime { get; set; }
+        bool IsReminderSent { get; set; }
+    }
+}
