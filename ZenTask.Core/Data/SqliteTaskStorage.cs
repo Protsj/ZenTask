88f55d12fbc7d582a99aab4701b9ee23f4eff15b
qrtm <artemprotsenkoyt@gmail.com>
2026-05-25 21:42:32 +0300

    feat: implement SQLite data storage and configure ModelBuilder

---
+++ ZenTask.Core/Data/SqliteTaskStorage.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenTask.Core.Models;
+
+namespace ZenTask.Core.Data
+{
+    public class SqliteTaskStorage
+    {
+        public async Task SaveAsync(IEnumerable<BaseTask> tasks)
+        {
+            using var context = new TaskDbContext();
+
+            foreach (var task in tasks)
+            {
+                bool exists = await context.Tasks.AnyAsync(t => t.Id == task.Id);
+                if (exists)
+                    context.Tasks.Update(task);
+                else
+                    await context.Tasks.AddAsync(task);
+            }
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<List<BaseTask>> LoadTasksAsync()
+        {
+            using var context = new TaskDbContext();
+            return await context.Tasks.ToListAsync();
+        }
+        public async Task DeleteTaskAsync(Guid id)
+        {
+            using var context = new TaskDbContext();
+            var taskToDelete = await context.Tasks.FindAsync(id);
+            if (taskToDelete != null)
+            {
+                context.Tasks.Remove(taskToDelete);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ ZenTask.Core/Data/TaskDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZenTask.Core.Models;
+
+namespace ZenTask.Core.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public DbSet<BaseTask> Tasks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=Tasks.db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseTask>().ToTable("Task");
+            modelBuilder.Entity<UrgentTask>().ToTable("UrgentTask");
+            modelBuilder.Entity<MeetingTask>().ToTable("MeetingTask");
+            modelBuilder.Entity<HabitTask>().ToTable("HabitTask");
+            modelBuilder.Entity<FocusTask>().ToTable("FocusTask");
+            modelBuilder.Entity<ListTask>().ToTable("ListTask");
+            modelBuilder.Entity<CallTask>().ToTable("CallTask");
+        }
+
+    }
+}
