f8443cc63dc43d0790cca855385b9a54b9049b8c
qrtm <artemprotsenkoyt@gmail.com>
2026-06-11 04:46:15 +0300

    fix: add check for reminder

--- ZenTask.Core/Models/UrgentTask.cs
+++ ZenTask.Core/Models/UrgentTask.cs
@@ -6,11 +6,13 @@ namespace ZenTask.Core.Models
     {
         public DateTime Deadline { get; set; }
         public bool IsCompleted { get; private set; }
+        public bool IsReminderSent { get; set; }
         public UrgentTask(string title, DateTime deadline, string description = "") 
             : base(title, description)
         {
             Deadline = deadline;
             IsCompleted = false;
+            IsReminderSent = false;
         }
         public void Complete() { IsCompleted = true; }
         public void UndoComplete() { IsCompleted = false; }
